89bb51ecfa17c861362c40483c3aaa64506c92ab
zhangting <187268218@qq.com>
2021-01-14 17:21:49 +0800

    20210114+1

--- src/main/java/leetcode/editor/cn/SearchInsertPosition.java
+++ src/main/java/leetcode/editor/cn/SearchInsertPosition.java
@@ -33,11 +33,23 @@ package leetcode.editor.cn;
 public class SearchInsertPosition {
     public static void main(String[] args) {
         Solution solution = new SearchInsertPosition().new Solution();
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},7));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int searchInsert(int[] nums, int target) {
-
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
